f5e78026149d93d5a565322a8b7dc20bf6b5e687
John Keiser <john@johnkeiser.com>
2020-09-03 12:30:17 -0700

    Add a "sum" benchmark with no appending to vector

--- benchmark/largerandom/ondemand.h
+++ benchmark/largerandom/ondemand.h
@@ -40,6 +40,38 @@ simdjson_really_inline bool OnDemand::Run(const padded_string &json) {
 
 BENCHMARK_TEMPLATE(LargeRandom, OnDemand);
 
+class OnDemandSum {
+public:
+  simdjson_really_inline bool Run(const padded_string &json);
+
+  simdjson_really_inline my_point &Result() { return sum; }
+  simdjson_really_inline size_t ItemCount() { return count; }
+
+private:
+  ondemand::parser parser{};
+  my_point sum{};
+  size_t count{};
+};
+
+simdjson_really_inline bool OnDemandSum::Run(const padded_string &json) {
+  sum = {0,0,0};
+  count = 0;
+
+  auto doc = parser.iterate(json);
+  // TODO this sucks, you should be able to just say for ( ... : doc)
+  auto array = doc.get_array();
+  for (ondemand::object coord : array) {
+    sum.x += double(coord["x"]);
+    sum.y += double(coord["y"]);
+    sum.z += double(coord["z"]);
+    count++;
+  }
+
+  return true;
+}
+
+BENCHMARK_TEMPLATE(LargeRandom, OnDemandSum);
+
 }
 } // namespace largerandom
 
